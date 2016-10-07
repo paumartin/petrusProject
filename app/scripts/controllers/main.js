6764c4361c23f29e31c08e440fe2f36056a02ff7
Pau Martin <39937860-Q@estudiants.urv.cat>
2016-10-07 18:04:18 +0200

    Angular app generated

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petrusProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the petrusProjectApp
+ */
+angular.module('petrusProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
